6b868f61e5f2e2b2365f4b18a690613554748b20
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-22 11:41:13 +0200

    Update main.cpp

--- homework/shared_ptr/main.cpp
+++ homework/shared_ptr/main.cpp
@@ -1,3 +1,13 @@
+#include "shared_ptr.hpp"
+
 int main() {
+    cs::shared_ptr<int> ptr(new int{5});
+    auto ptr2(ptr);
+    cs::shared_ptr<int> ptr3(new int{5});
+    ptr3 = ptr;
+    auto ptr4(std::move(ptr));
+    cs::shared_ptr<int> ptr5(new int{5});
+    ptr5 = std::move(ptr3);
+
     return 0;
 }
