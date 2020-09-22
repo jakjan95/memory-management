9eb65af66bec8dbeb87e72e8a03bf0c6ec513908
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-22 11:50:58 +0200

    Added dereference test case

--- homework/shared_ptr/shared_ptr-ut.cpp
+++ homework/shared_ptr/shared_ptr-ut.cpp
@@ -1,3 +1,16 @@
 #include <gtest/gtest.h>
 
 #include "shared_ptr.hpp"
+
+constexpr int initialValue = 5;
+
+struct SharedPtrTest : ::testing::Test {
+    SharedPtrTest()
+        : ptr(new int{initialValue}) {}
+
+    cs::shared_ptr<int> ptr;
+};
+
+TEST_F(SharedPtrTest, shouldDereferenceSharedPtr) {
+    ASSERT_EQ(*ptr, initialValue);
+}
