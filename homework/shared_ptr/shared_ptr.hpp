7bf26b3a9de8c019e727235ba9dd9ad917a6f53a
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-22 11:41:02 +0200

    Rule of 5

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -1,7 +1,88 @@
 #pragma once
 
+#include "sharedControlBlock.hpp"
+
 namespace cs {
 template <typename T>
 class shared_ptr {
+public:
+    constexpr shared_ptr() noexcept = default;
+    constexpr shared_ptr(std::nullptr_t) noexcept {}
+    explicit shared_ptr(T* ptr)
+        : ptr_(ptr), controlBlock_(new SharedControlBlock{}) {}
+
+    shared_ptr(const shared_ptr& other) noexcept;
+    shared_ptr(shared_ptr&& other) noexcept;
+    ~shared_ptr();
+
+    shared_ptr& operator=(const shared_ptr& other) noexcept;
+    shared_ptr& operator=(shared_ptr&& other) noexcept;
+
+private:
+    T* ptr_ = nullptr;
+    SharedControlBlock* controlBlock_ = nullptr;
 };
+
+template <typename T>
+shared_ptr<T>::shared_ptr(const shared_ptr<T>& other) noexcept {
+    ptr_ = other.ptr_;
+    other.controlBlock_->incrementSharedRefs();
+    controlBlock_ = other.controlBlock_;
+}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(shared_ptr&& other) noexcept {
+    ptr_ = other.ptr_;
+    controlBlock_ = other.controlBlock_;
+    other.ptr_ = nullptr;
+    other.controlBlock_ = nullptr;
+}
+
+template <typename T>
+shared_ptr<T>::~shared_ptr() {
+    if (controlBlock_) {
+        if (controlBlock_->getSharedRefs() == 1) {
+            delete ptr_;
+            delete controlBlock_;
+        } else {
+            controlBlock_->decrementSharedRefs();
+        }
+    }
+}
+
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<T>& other) noexcept {
+    if (controlBlock_) {
+        if (controlBlock_->getSharedRefs() == 1) {
+            delete ptr_;
+            delete controlBlock_;
+        } else {
+            controlBlock_->decrementSharedRefs();
+        }
+    }
+
+    ptr_ = other.ptr_;
+    other.controlBlock_->incrementSharedRefs();
+    controlBlock_ = other.controlBlock_;
+    return *this;
+}
+
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr<T>&& other) noexcept {
+    if (controlBlock_) {
+        if (controlBlock_->getSharedRefs() == 1) {
+            delete ptr_;
+            delete controlBlock_;
+        } else {
+            controlBlock_->decrementSharedRefs();
+        }
+    }
+
+    ptr_ = other.ptr_;
+    controlBlock_ = other.controlBlock_;
+    other.ptr_ = nullptr;
+    other.controlBlock_ = nullptr;
+    return *this;
+}
+
 }  // namespace cs
